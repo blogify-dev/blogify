c00ef23af81fb4577a9f5aa315cad937b252dcbe
lucya <lucy.agamaite@gmail.com>
2019-08-13 21:14:54 -0400

    implemented user class

--- src/services/UserService.kt
+++ src/services/UserService.kt
@@ -1,5 +1,6 @@
 package me.benjozork.services
 
+import me.benjozork.resources.Article
 import me.benjozork.resources.User
 import me.benjozork.resources.models.Resource
 import me.benjozork.services.models.Service
@@ -8,24 +9,31 @@ import java.util.*
 
 class UserService : Service<User> {
 
+    private val userData = mutableSetOf(
+        User(name = "Shrek"),
+        User(name = "Keanu Reeves")
+    ).associateBy { it.uuid }.toMutableMap()
+
     override fun getAll(): Set<User> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return userData.values.toSet()
     }
 
-    override fun get(id: UUID): User {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun get(id: UUID): User? {
+        return userData[id]
     }
 
     override fun add(res: User): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData[res.uuid] = res
+        return true
     }
 
     override fun remove(id: UUID): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData.remove(id) ?: return false
+        return true
     }
 
     override fun update(usr: User): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        userData[usr.uuid] = usr
+        return true    }
 
 }
