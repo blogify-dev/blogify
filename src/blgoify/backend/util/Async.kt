bc691c5f4c3e411b2f3f472a5008cd2a57ba2117
Benjamin Dupont <benjozorkfr@gmail.com>
2019-08-19 00:45:50 -0400

    async query tweaks

--- src/blgoify/backend/util/Async.kt
+++ src/blgoify/backend/util/Async.kt
@@ -3,6 +3,9 @@ package blgoify.backend.util
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
-suspend fun <T> query(block: () -> T): T = withContext(Dispatchers.IO) { transaction { block() } }
+/**
+ * DO NOT USE THIS IN A NESTED QUERY ! EXPOSED BUG
+ */
+suspend fun <T> query(block: suspend () -> T): T = withContext(Dispatchers.IO) { newSuspendedTransaction { block() } }
