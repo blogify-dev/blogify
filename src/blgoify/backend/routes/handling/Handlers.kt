1b4c94ecc34e60731bf87a9efeb45b1365d74630
Benjamin Dupont <benjozorkfr@gmail.com>
2019-08-21 08:24:36 -0400

    handler improvements :)

--- src/blgoify/backend/routes/handling/Handlers.kt
+++ src/blgoify/backend/routes/handling/Handlers.kt
@@ -1,19 +1,29 @@
 package blgoify.backend.routes.handling
 
-import blgoify.backend.resources.models.Resource
-import blgoify.backend.util.toUUID
-
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.ContentTransformationException
-import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.util.pipeline.PipelineContext
 
+import blgoify.backend.resources.models.Resource
+import blgoify.backend.util.toUUID
+
 import java.util.UUID
 
-suspend fun <R : Resource> PipelineContext<Unit, ApplicationCall>.handleSimpleResourceFetch (
+/**
+ * Represents a server call pipeline
+ */
+typealias CallPipeline = PipelineContext<Unit, ApplicationCall>
+
+/**
+ * Adds a handler to a [CallPipeline] that handles fetching a resource.
+ *
+ * @param R         the type of [Resource] to be fetched
+ * @param fetch     the [function][Function] that retrieves the resource
+ * @param transform a transformation [function][Function] that transforms the [resource][Resource] before sending it back to the client
+ */
+suspend fun <R : Resource> CallPipeline.handleResourceFetch (
     fetch:     suspend (id: UUID)   -> R?,
     transform: suspend (fetched: R) -> Any = { it }
 ) {
@@ -24,8 +34,42 @@ suspend fun <R : Resource> PipelineContext<Unit, ApplicationCall>.handleSimpleRe
     } ?: call.respond(HttpStatusCode.BadRequest)
 }
 
-@Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
-suspend inline fun <reified R : Resource> PipelineContext<Unit, ApplicationCall>.handleSimpleResourceCreation (
+/**
+ * Adds a handler to a [CallPipeline] that handles fetching all the available resources.
+ *
+ * @param R         the type of [Resource] to be fetched
+ * @param fetch     the [function][Function] that retrieves the resources
+ * @param transform a transformation [function][Function] that transforms the [resources][Resource] before sending them back to the client
+ */
+suspend fun <R : Resource> CallPipeline.handleResourceFetchAll (
+    fetch:     suspend ()        -> Collection<R>,
+    transform: suspend (elem: R) -> Any = { it }
+) {
+    fetch.invoke().takeIf { it.isNotEmpty() }?.let { set ->
+        call.respond(set.map { transform.invoke(it) })
+    } ?: call.respond(HttpStatusCode.NoContent)
+}
+
+/**
+ * Adds a handler to a [CallPipeline] that handles fetching all the available resources that are related to a particular resource.
+ *
+ * @param R         the type of [Resource] to be fetched
+ * @param fetch     the [function][Function] that retrieves the resources using the [ID][UUID] of another resource
+ * @param transform a transformation [function][Function] that transforms the [resources][Resource] before sending them back to the client
+ */
+suspend fun <R : Resource> CallPipeline.handleIdentifiedResourceFetchAll (
+    fetch:     suspend (id: UUID) -> Collection<R>,
+    transform: suspend (elem: R)  -> Any = { it }
+) {
+    call.parameters["uuid"]?.let { id ->
+        fetch.invoke(id.toUUID()).takeIf { it.isNotEmpty() }?.let { set ->
+            call.respond(set.map { transform.invoke(it) })
+        } ?: call.respond(HttpStatusCode.NoContent)
+    } ?: call.respond(HttpStatusCode.NotFound)
+}
+
+/* @Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
+suspend inline fun <reified R : Resource> CallPipeline.handleSimpleResourceCreation (
     creationFunction: suspend (res: R) -> Boolean
 ) {
     try {
@@ -39,4 +83,4 @@ suspend inline fun <reified R : Resource> PipelineContext<Unit, ApplicationCall>
     } catch (e: ContentTransformationException) {
         call.respond(HttpStatusCode.BadRequest)
     }
-}
+} // KT-33440 | Doesn't compile when called for now */
