5ea5dfc5ce867b6136cc5f1f4e52e8931e4477b0
Benjamin Dupont <benjozorkfr@gmail.com>
2019-08-23 16:57:07 -0400

    IMPORTS

--- src/blgoify/backend/routes/AuthRoutes.kt
+++ src/blgoify/backend/routes/AuthRoutes.kt
@@ -1,13 +1,10 @@
 package blgoify.backend.routes
 
-import blgoify.backend.resources.User
-import blgoify.backend.services.UserService
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 
