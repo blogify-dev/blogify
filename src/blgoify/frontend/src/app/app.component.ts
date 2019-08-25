28528a3994f5002df9b4126cdea65e98d11b2105
lucya <lucy.agamaite@gmail.com>
2019-08-25 17:41:52 -0400

    content display

--- src/blgoify/frontend/src/app/app.component.ts
+++ src/blgoify/frontend/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { ArticleService } from './services/articles/article.service';
 import { UsersService } from './services/users/users.service';
 import { CommentsService } from './services/comments/comments.service';
+import {ContentService} from "./services/content/content.service";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css', './styles.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'blogify';
 
     private urlArticles = '/api/articles';
@@ -21,6 +22,7 @@ export class AppComponent {
     articles;
     users;
     comments;
+    content;
     headers = new HttpHeaders()
         .append('Content-Type', 'application/json');
 
@@ -28,13 +30,27 @@ export class AppComponent {
         private http: HttpClient,
         private articleService: ArticleService,
         private usersService: UsersService,
-        private commnetsService: CommentsService) { }
+        private commentsService: CommentsService,
+        private contentService: ContentService) { }
+
+    ngOnInit(){
+      this.getArticles().then(r => Promise);
+      this.getComments().then(r => Promise);
+      this.getUsers().then(r=> Promise);
+      this.getContent().then(r=> Promise);
+    }
 
     async getArticles() {
         const articlesob = this.articleService.getAllArticles();
         this.articles = await articlesob.toPromise();
         console.log(this.articles);
         return this.articles;
+    }
+
+    async getContent(){
+      const uuid = '85a1841f-143e-44cd-ad55-347207a60726';
+      this.content = await this.contentService.getContent(uuid).toPromise();
+      console.log(this.content);
     }
 
     async getUsers() {
@@ -44,7 +60,7 @@ export class AppComponent {
 
     async getComments() {
         const uuid = '85a1841f-143e-44cd-ad55-347207a60726';
-        this.comments = await this.commnetsService.getCommentsForArticle(uuid).toPromise();
+        this.comments = await this.commentsService.getCommentsForArticle(uuid).toPromise();
         console.log(this.comments);
     }
 }
---
+++ src/blgoify/frontend/src/app/services/content/content.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+  private contentEndpoint = '/api/content';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getContent(uuid: string) {
+    return this.httpClient.get(`${this.contentEndpoint}/${uuid}`);
+  }
+
+}
