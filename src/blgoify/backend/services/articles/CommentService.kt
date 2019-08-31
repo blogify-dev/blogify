21e9628c396599d304ac24b59c2e9fc51fe1b8ce
Benjamin Dupont <benjozorkfr@gmail.com>
2019-08-31 10:19:26 -0400

    comment service now uses db handlers

--- src/blgoify/backend/services/articles/CommentService.kt
+++ src/blgoify/backend/services/articles/CommentService.kt
@@ -3,39 +3,25 @@ package blgoify.backend.services.articles
 import blgoify.backend.resources.Comment
 import blgoify.backend.services.models.Service
 import blgoify.backend.util.query
-import blgoify.backend.database.Comments.convert
 import blgoify.backend.database.Comments
 import blgoify.backend.database.Comments.uuid
+import blgoify.backend.services.handling.handleResourceDBFetch
+import blgoify.backend.services.handling.handleResourceDBFetchAll
 import blgoify.backend.services.models.ResourceResult
 import blgoify.backend.services.models.ResourceResultSet
 
-import com.github.kittinunf.result.coroutines.map
 import com.github.kittinunf.result.coroutines.mapError
 
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
 
 import java.util.UUID
 
 object CommentService : Service<Comment> {
 
-    override suspend fun getAll(): ResourceResultSet<Comment> {
-        return query {
-            Comments.selectAll().toSet()                            // Poll the database
-        }
-            .map      { r -> r.map { convert(it).get() }.toSet() }  // If the query returns a successful result set, convert it
-            .mapError { e -> Service.Exception.Fetching(e) }        // Otherwise, wrap the error in a Service.Exception.Fetching
-    }
+    override suspend fun getAll(): ResourceResultSet<Comment> = handleResourceDBFetchAll(Comments)
 
-    override suspend fun get(id: UUID): ResourceResult<Comment> {
-        return query {
-            Comments.select { uuid eq id }.single()          // Poll the database
-        }
-            .map      { r -> convert(r).get() }              // If the query returns a successful result, convert it
-            .mapError { e -> Service.Exception.Fetching(e) } // Otherwise, wrap the error in a Service.Exception.Fetching
-    }
+    override suspend fun get(id: UUID): ResourceResult<Comment> = handleResourceDBFetch(Comments, uuid, id)
 
     override suspend fun add(res: Comment) = query {
         Comments.insert {
