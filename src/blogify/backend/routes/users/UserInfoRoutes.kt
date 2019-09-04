202693eae19259c4639dbb1f3199da46387c4e14
benjozork <benjozorkfr@gmail.com>
2019-09-04 09:54:39 -0400

    formatting

--- src/blogify/backend/routes/users/UserInfoRoutes.kt
+++ src/blogify/backend/routes/users/UserInfoRoutes.kt
@@ -1,9 +1,10 @@
 package blogify.backend.routes.users
 
-import blogify.backend.routes.handling.handleResourceFetch
-import blogify.backend.services.UserService
 import io.ktor.routing.Route
 import io.ktor.routing.get
+
+import blogify.backend.routes.handling.handleResourceFetch
+import blogify.backend.services.UserService
 
 fun Route.userInfo() {
 
