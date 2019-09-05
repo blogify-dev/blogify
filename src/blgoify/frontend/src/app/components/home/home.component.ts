3d8c02b3ecb1051aaf307fc638ef405a0f2f9e9c
Hamza <muhammadhamza1311@gmail.com>
2019-09-05 22:02:54 +0500

    Home component now displays all articles

--- src/blgoify/frontend/src/app/components/home/home.component.ts
+++ src/blgoify/frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from 'src/app/services/article/article.service';
 import { Article } from '../../models/Article'
-import { Router } from '@angular/router';
-import { LoginComponent } from "../login/login.component";
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'blogify';
+    title = 'blogify';
+    articles: Article[];
+
+    constructor(private articleService: ArticleService) {
+    }
 
 
-
-ngOnInit(): void {
-}
-
+    ngOnInit() {
+        this.articleService.getAllArticles().subscribe(it => {
+            this.articles = it
+        })
+    }
 
 
 }
