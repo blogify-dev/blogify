5d81261e2df01a449ebfc54670873011fc187ae0
Benjamin Dupont <benjozorkfr@gmail.com>
2019-09-14 14:33:25 -0400

    styling and layout improvements

--- src/blgoify/frontend/src/app/components/home/home.component.ts
+++ src/blgoify/frontend/src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ArticleService } from 'src/app/services/article/article.service';
 import { Article } from '../../models/Article'
 
-
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
