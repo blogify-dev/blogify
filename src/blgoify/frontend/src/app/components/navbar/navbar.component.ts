57fc8225c3b47c15c124e9e4484da149ace8e935
Hamza <muhammadhamza1311@gmail.com>
2019-09-15 20:27:49 +0500

    *ngIf-ed profile and login

--- src/blgoify/frontend/src/app/components/navbar/navbar.component.ts
+++ src/blgoify/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+      console.log(this.authService.userToken)
   }
 
 }
