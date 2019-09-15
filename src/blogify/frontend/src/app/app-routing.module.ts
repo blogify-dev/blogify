d395a4d140d023cda29620ca70d27fc53573f7c7
Benjamin Dupont <benjozorkfr@gmail.com>
2019-09-15 14:26:17 -0400

    fixed files again and again

---
+++ src/blogify/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {SearchComponent} from "./components/search/search.component";
+import {NewArticleComponent} from "./components/newarticle/new-article.component";
+import { ShowArticleComponent } from "./components/show-article/show-article.component";
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'search', component: SearchComponent },
+    { path: 'new-article', component: NewArticleComponent },
+    { path: 'profile/:uuid', component: ProfileComponent },
+    { path: 'article/:uuid', component: ShowArticleComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/blogify/frontend/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from "@angular/router";
+import { NewArticleComponent } from './components/newarticle/new-article.component';
+import { SearchComponent } from './components/search/search.component';
+import { ShowArticleComponent } from './components/show-article/show-article.component';
+import { ShowAllArticlesComponent } from './components/show-all-articles/show-all-articles.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        ProfileComponent,
+        HomeComponent,
+        NewArticleComponent,
+        SearchComponent,
+        ShowArticleComponent,
+        ShowAllArticlesComponent,
+        CommentComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
