b0e575d50032718e4223609f8fc5db4dfc3d9190
Hamza <muhammadhamza1311@gmail.com>
2019-09-15 20:44:49 +0500

    Fixed navbar code

--- src/blgoify/frontend/src/app/components/navbar/navbar.component.ts
+++ src/blgoify/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../services/auth/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) {
+    }
 
-  ngOnInit() {
-      console.log(this.authService.userToken)
-  }
+    ngOnInit() {
+        console.log(this.authService.userToken)
+    }
 
+    async navigateToProfile() {
+        const url = `/profile/${await this.authService.getUserUUID(this.authService.userToken)}`
+        await this.router.navigateByUrl(url)
+    }
+
+    async navigateToNewArticle() {
+        if (this.authService.userToken == '') {
+            await this.router.navigateByUrl('/login')
+        } else {
+            await this.router.navigateByUrl('/new-article')
+
+        }
+    }
 }
