f7a63b125a5e813ed923d6426f0183f7cebb1b0a
Hamza <muhammadhamza1311@gmail.com>
2019-09-22 12:38:52 +0500

    Forgot to add in last commit

---
+++ src/blogify/frontend/src/app/directives/dark-theme/dark-theme.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DarkThemeDirective } from './dark-theme.directive';
+
+describe('DarkThemeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DarkThemeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
