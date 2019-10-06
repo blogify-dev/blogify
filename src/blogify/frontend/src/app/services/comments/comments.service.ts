9830616eb1e3b2758e5ca710155967eca0191ca7
Hamza <muhammadhamza1311@gmail.com>
2019-10-06 23:12:29 +0500

    Comment service fixes (untested)

--- src/blogify/frontend/src/app/services/comments/comments.service.ts
+++ src/blogify/frontend/src/app/services/comments/comments.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Comment } from '../../models/Comment';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Comment} from '../../models/Comment';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
     providedIn: 'root'
@@ -8,18 +9,31 @@ import { Comment } from '../../models/Comment';
 export class CommentsService {
     private commentsEndpoint = '/api/articles/comments';
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient, private authService: AuthService) {
     }
 
     getCommentsForArticle(articleUUID: string) {
         return this.httpClient.get<Comment[]>(`${this.commentsEndpoint}/${articleUUID}`);
     }
 
-    deleteComment(commentUUID: string) {
-        return this.httpClient.delete(`${this.commentsEndpoint}/${commentUUID}`);
+    deleteComment(commentUUID: string, userToken: string = this.authService.userToken) {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userToken}`
+            })
+        };
+
+        return this.httpClient.delete(`${this.commentsEndpoint}/${commentUUID}`, httpOptions);
     }
 
-    createComment(comment: Comment) {
-        return this.httpClient.post(`${this.commentsEndpoint}`, comment);
+    createComment(comment: Comment, userToken: string = this.authService.userToken) {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userToken}`
+            })
+        };
+        return this.httpClient.post(`${this.commentsEndpoint}`, comment, httpOptions);
     }
 }
