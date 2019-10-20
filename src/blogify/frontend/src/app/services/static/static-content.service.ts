f2884dc59d92c0667a6659a7cf69015126100573
Hamza <muhammadhamza1311@gmail.com>
2019-10-20 22:41:44 +0500

    User pfp works on frontend

--- src/blogify/frontend/src/app/services/static/static-content.service.ts
+++ src/blogify/frontend/src/app/services/static/static-content.service.ts
@@ -9,7 +9,7 @@ export class StaticContentService {
     constructor() {}
 
     urlFor(file: StaticFile): string {
-      return `http://192.168.122.56:8080/api/get/${file.collection}/${file.id}`
+      return `/api/get/${file.collection}/${file.id}`
     }
 
 }
