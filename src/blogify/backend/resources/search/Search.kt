6cdc6c3f913b25aee60d1fd1cda6e13da35b4c00
Benjamin Dupont <benjozorkfr@gmail.com>
2019-11-03 00:52:13 -0400

    search: kdoc stubs for new classes, formatting

--- src/blogify/backend/resources/search/Search.kt
+++ src/blogify/backend/resources/search/Search.kt
@@ -2,36 +2,54 @@ package blogify.backend.resources.search
 
 import blogify.backend.resources.models.Resource
 
-data class Search<S : Resource>(
-    val _shards: Shards,
-    val hits: Hits<S>,
+/**
+ * TODO complete doc
+ *
+ * @author hamza1311
+ */
+data class Search<S : Resource> (
+    val _shards:   Shards,
+    val hits:      Hits<S>,
     val timed_out: Boolean,
-    val took: Int
+    val took:      Int
 ) {
-    data class Shards(
-        val failed: Int,
-        val skipped: Int,
+
+    /**
+     * TODO complete doc
+     */
+    data class Shards (
+        val failed:     Int,
+        val skipped:    Int,
         val successful: Int,
-        val total: Int
+        val total:      Int
     )
 
-    data class Hits<S : Resource>(
-        val hits: List<Hit<S>>,
+    /**
+     * TODO complete doc
+     */
+    data class Hits<S : Resource> (
+        val hits:      List<Hit<S>>,
         val max_score: Double,
-        val total: Total
+        val total:     Total
     )
 
-    data class Hit<S : Resource>(
-        val _id: String,
-        val _index: String,
-        val _score: Double,
+    /**
+     * TODO complete doc
+     */
+    data class Hit<S : Resource> (
+        val _id:     String,
+        val _index:  String,
+        val _score:  Double,
         val _source: S,
-        val _type: String
+        val _type:   String
     )
 
-    data class Total(
+    /**
+     * TODO complete doc
+     */
+    data class Total (
         val relation: String,
-        val value: Int
+        val value:    Int
     )
 
 }
--- src/blogify/backend/routes/pipelines/Pipelines.kt
+++ src/blogify/backend/routes/pipelines/Pipelines.kt
@@ -14,6 +14,7 @@ import io.ktor.util.pipeline.PipelineContext
 import io.ktor.util.pipeline.PipelineInterceptor
 
 import com.andreapivetta.kolor.red
+
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("blogify-pipeline-manager")
