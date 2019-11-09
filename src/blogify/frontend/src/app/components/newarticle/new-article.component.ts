ae0c9c7f23c8d035ae8d4a6c526e3fbad406bc34
Benjamin Dupont <benjozorkfr@gmail.com>
2019-11-09 17:30:15 -0500

    validation: made new-article a form

--- src/blogify/frontend/src/app/components/newarticle/new-article.component.ts
+++ src/blogify/frontend/src/app/components/newarticle/new-article.component.ts
@@ -4,6 +4,8 @@ import { ArticleService } from '../../services/article/article.service';
 import { User } from '../../models/User';
 import { StaticFile } from "../../models/Static";
 import { AuthService } from '../../shared/auth/auth.service';
+import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 @Component({
     selector: 'app-new-article',
@@ -23,19 +25,42 @@ export class NewArticleComponent implements OnInit {
     };
     user: User;
 
+    datePipe = new DatePipe('en-US');
+
+    formTitle =      new FormControl('', [Validators.required]);
+    formSummary =    new FormControl('', [Validators.required]);
+    formContent =    new FormControl('', [Validators.required]);
+    formCategories = new FormArray([new FormControl('')]);
+
+    form = new FormGroup({
+        'title':      this.formTitle,
+        'summary':    this.formSummary,
+        'content':    this.formContent,
+        'categories': this.formCategories
+    });
+
     constructor(private articleService: ArticleService, private authService: AuthService) {}
 
     async ngOnInit() {
         this.user = await this.authService.userProfile;
     }
 
+    transformArticleData(input: object): object {
+        input['categories'] = input['categories'].map(cat => { return { name: cat }});
+        return input
+    }
+
     createNewArticle() {
-        this.articleService.createNewArticle(this.article).then(article =>
-            console.log(article)
+        alert('a');
+        this.articleService.createNewArticle (
+            (<Article> this.transformArticleData(this.form.value))
+        ).then(article =>
+            console.warn(article)
         );
     }
 
     addCategory() {
-        this.article.categories.push({name: ''});
+        this.formCategories.push(new FormControl(''));
     }
+
 }
