78c32c5514e555bb4c6d39dce65c17c583d88ca6
Hamza <muhammadhamza1311@gmail.com>
2019-11-12 19:39:36 +0500

    Get rid of useless static routes

--- src/blogify/backend/routes/StaticRoutes.kt
+++ src/blogify/backend/routes/StaticRoutes.kt
@@ -28,15 +28,6 @@ import com.github.kittinunf.result.coroutines.map
 
 fun Route.static() {
 
-    post("/testupload/{uuid}") {
-        uploadToResource (
-            fetch         = UserService::get,
-            modify        = { r, h -> r.copy(profilePicture = h) },
-            update        = UserService::update,
-            authPredicate = { user, manipulated -> user eqr manipulated }
-        )
-    }
-
     get("/get/{uploadableId}") {
 
         pipeline("uploadableId") { (uploadableId) ->
@@ -51,31 +42,6 @@ fun Route.static() {
             }
 
         }
-
-    }
-
-    delete("/delete/{uploadableId}") {
-
-        val doDelete: CallPipeLineFunction = { pipeline("uploadableId") { (uploadableId) ->
-            // TODO: None of this should be executed unless the owner is logged in. Fix that
-            // not-so VERY TEMP
-            val handle = query {
-                Uploadables.select { Uploadables.fileId eq uploadableId }.single()
-            }.map { Uploadables.convert(call, it).get() }.get()
-
-            // Delete in DB
-            query {
-                Uploadables.deleteWhere { Uploadables.fileId eq uploadableId }
-            }.failure { pipelineError(HttpStatusCode.InternalServerError, "couldn't delete static resource from db") }
-
-            // Delete in FS
-            if (StaticFileHandler.deleteStaticResource(handle)) {
-                call.respond(HttpStatusCode.OK)
-            } else pipelineError(HttpStatusCode.InternalServerError, "couldn't delete static resource file")
-
-        } }
-
-        handleAuthentication("resourceDelete", { _ -> true }, doDelete)
 
     }
 
