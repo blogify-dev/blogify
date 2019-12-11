495e738dfb4c317b593e0b8c136b765309844bef
Benjamin Dupont <benjozorkfr@gmail.com>
2019-12-11 18:19:07 -0500

    results: fix

--- src/blogify/backend/search/ext/SearchExt.kt
+++ src/blogify/backend/search/ext/SearchExt.kt
@@ -4,14 +4,14 @@ import blogify.backend.resources.models.Resource
 import blogify.backend.resources.reflect.sanitize
 import blogify.backend.search.models.Search
 import blogify.backend.search.models.SearchView
-import blogify.backend.services.models.ResourceResult
+import blogify.backend.util.Sr
 import blogify.backend.util.service
 import blogify.backend.util.toUUID
 
 import java.lang.Exception
 import java.lang.IllegalStateException
 
-suspend inline fun <reified T : Resource> Search.Hit.fetchResource(): ResourceResult<T> {
+suspend inline fun <reified T : Resource> Search.Hit.fetchResource(): Sr<T> {
     val resourceUUID = (this.document["uuid"] as String).toUUID()
     return T::class.service.get(Resource.ObjectResolver.FakeApplicationCall, resourceUUID)
 }
