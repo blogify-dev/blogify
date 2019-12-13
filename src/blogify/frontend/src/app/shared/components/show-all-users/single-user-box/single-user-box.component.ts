5c9ab55243f5f631a12ba23387a5972fccf563ea
Hamza <muhammadhamza1311@gmail.com>
2019-12-13 18:33:03 +0500

    Show all users: single-user-box component UI

--- src/blogify/frontend/src/app/shared/components/show-all-users/single-user-box/single-user-box.component.ts
+++ src/blogify/frontend/src/app/shared/components/show-all-users/single-user-box/single-user-box.component.ts
@@ -1,4 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../../../models/User';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
     selector: 'app-single-user-box',
@@ -6,12 +9,19 @@ import {Component, Input, OnInit} from '@angular/core';
     styleUrls: ['./single-user-box.component.scss']
 })
 export class SingleUserBoxComponent implements OnInit {
-    @Input() user;
+    @Input() user: User;
 
-    constructor() {
-    }
+    faCopy = faCopy;
+
+    constructor(
+        private clipboardService: ClipboardService,
+    ) { }
 
     ngOnInit() {
+    }
+
+    copyLinkToClipboard() {
+        this.clipboardService.copyFromContent(window.location.href);
     }
 
 }
