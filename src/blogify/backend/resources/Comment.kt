4845346e250f7b39a60c863a8bd092fd6fa47257
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-02-15 17:58:07 -0500

    repos: add missing @SqlTable annotations

--- src/blogify/backend/resources/Comment.kt
+++ src/blogify/backend/resources/Comment.kt
@@ -1,15 +1,16 @@
 package blogify.backend.resources
 
+import blogify.backend.annotations.SqlTable
 import blogify.backend.annotations.search.NoSearch
 import blogify.backend.database.Comments
 import blogify.backend.database.referredToBy
 import blogify.backend.resources.computed.compound
 import blogify.backend.resources.computed.models.Computed
+import blogify.backend.resources.models.Resource
+
 import com.fasterxml.jackson.annotation.JsonIdentityReference
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
-
-import blogify.backend.resources.models.Resource
 
 import java.util.UUID
 
@@ -19,7 +20,8 @@ import java.util.UUID
     generator = ObjectIdGenerators.PropertyGenerator::class,
     property = "uuid"
 )
-data class Comment(
+@SqlTable(Comments::class)
+data class Comment (
     @JsonIdentityReference(alwaysAsId = true)
     val commenter: User,
 
