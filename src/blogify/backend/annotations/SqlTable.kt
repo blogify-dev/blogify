83fb9416015c27e496e7fc4d5b2ba1c7c93140bd
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-02-15 20:39:06 -0500

    doc: document SqlTable and RClass table prop

--- src/blogify/backend/annotations/SqlTable.kt
+++ src/blogify/backend/annotations/SqlTable.kt
@@ -6,10 +6,20 @@ import blogify.backend.resources.models.Resource
 import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
 
+/**
+ * Associates a [Resource] class to a [ResourceTable] when using a [blogify.backend.persistence.PostgresDataStore]
+ *
+ * @param table the class of the table in which instances of this class are stored
+ *
+ * @author Benjozork
+ */
 annotation class SqlTable (
     val table: KClass<out ResourceTable<*>>
 )
 
+/**
+ * Computed value that finds the [ResourceTable] for a given subclass of [Resource] using the [SqlTable] annotation
+ */
 val <R : Resource> KClass<R>.table: ResourceTable<R>
     get() = (this.findAnnotation<SqlTable>()?.table?.objectInstance ?: error("no @SqlTable annotation on class ${this.simpleName}"))
             as ResourceTable<R>
