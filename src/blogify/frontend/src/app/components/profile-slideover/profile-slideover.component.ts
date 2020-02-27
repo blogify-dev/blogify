269d2072ef4d1f3060a782fd58bc20ccc7748754
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-02-26 20:04:03 -0500

    slideover: more work, disable for 0.3.0

---
+++ src/blogify/frontend/src/app/components/profile-slideover/profile-slideover.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/auth/auth.service';
+import { User } from '../../models/User';
+import { faList, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+@Component({
+    selector: 'app-profile-slideover',
+    templateUrl: './profile-slideover.component.html',
+    styleUrls: ['./profile-slideover.component.scss']
+})
+export class ProfileSlideoverComponent implements OnInit {
+
+    user: User;
+
+    faList = faList;
+    faUser = faUser;
+
+    faSignOutAlt = faSignOutAlt;
+
+    constructor (
+        public authService: AuthService
+    ) {}
+
+    async ngOnInit() {
+        this.user = await this.authService.userProfile;
+    }
+
+}
