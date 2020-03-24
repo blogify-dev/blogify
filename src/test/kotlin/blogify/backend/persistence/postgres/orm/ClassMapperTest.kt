43c2d5a769ca755396ce624884c33e96ce86994f
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-03-24 15:43:05 -0400

    orm: tests: flesh out a bit more

--- src/test/kotlin/blogify/backend/persistence/postgres/orm/ClassMapperTest.kt
+++ src/test/kotlin/blogify/backend/persistence/postgres/orm/ClassMapperTest.kt
@@ -1,13 +1,14 @@
 package blogify.backend.persistence.postgres.orm
 
 import blogify.backend.annotations.Invisible
+import blogify.backend.persistence.postgres.orm.annotations.Cardinality
+import blogify.backend.persistence.postgres.orm.models.OrmTable
 import blogify.backend.persistence.postgres.orm.models.PropertyMapping
-import blogify.backend.resources.Article
-import blogify.backend.resources.Comment
 import blogify.backend.resources.models.Resource
-import org.jetbrains.exposed.sql.Column
 
+import org.jetbrains.exposed.sql.ForeignKeyConstraint
 import org.jetbrains.exposed.sql.IntegerColumnType
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.TextColumnType
 import org.jetbrains.exposed.sql.UUIDColumnType
 
@@ -23,9 +24,11 @@ class ClassMapperTest {
     ) : Resource()
 
     @Test fun `should map simple class with only values properly`() {
-        val table = ClassMapper.mapClasses(TestClass::class, ComplexTestClass::class).first()
+        val tables = ClassMapper.mapClasses(TestClass::class, ComplexTestClass::class)
 
-        println(table.columns.map { "${it.columnType::class.simpleName} - ${it.name}"})
+        tables.forEach { println(dumpOrmTable(it)) }
+
+        val table = tables.toList().first()
 
         assertEquals(1, table.mappings.count { it is PropertyMapping.IdentifierMapping })
         assertEquals(3, table.mappings.count { it is PropertyMapping.ValueMapping })
@@ -46,7 +49,7 @@ class ClassMapperTest {
     @Test fun `should map complex class with values and associations properly`() {
         val table = ClassMapper.mapSingleClass(ComplexTestClass::class)
 
-        println(table.columns.map { "${it.columnType::class.simpleName} - ${it.name}"})
+        println(dumpOrmTable(table))
 
         assertEquals(1, table.mappings.count { it is PropertyMapping.IdentifierMapping })
         assertEquals(3, table.mappings.count { it is PropertyMapping.ValueMapping })
@@ -72,6 +75,8 @@ class ClassMapperTest {
         val testClassTable = tables[0]
         val complexClassTable = tables[1]
 
+        tables.forEach { println(dumpOrmTable(it)) }
+
         ClassMapper.resolveAssociativeMappings(complexClassTable)
         assertEquals(0, complexClassTable.dependencyTables.size)
 
@@ -88,9 +93,45 @@ class ClassMapperTest {
         assertEquals(assocColumn.second, assocColumnFk.from)
         assertEquals(testClassTable.identifyingColumn, assocColumnFk.target)
 
-        println("Columns : " + complexClassTable.columns.map { "${it.columnType::class.simpleName} - ${it.name}"})
-        println("FKs : " + complexClassTable.columns.filter { it.foreignKey != null }
-            .map { it.foreignKey?.let { fk -> "${fk.from.table.tableName}[${fk.from.name}] -> ${fk.target.table.tableName}[${fk.target.name}]" } })
+    }
+
+    private data class TestClassCollections (
+        val name: String,
+        val age: Int,
+        val tests: Set<@Cardinality(PropertyMapping.AssociativeMapping.CollectionCardinality.MANY_TO_MANY) TestClass>,
+        @Invisible val password: String
+    ) : Resource()
+
+    @Test fun `should resolve associative mappings with collection properties properly`() {
+        val table = ClassMapper.mapClasses(TestClass::class, TestClassCollections::class).toList()[1]
+
+        println(dumpOrmTable(table))
+    }
+
+    private fun dumpOrmTable(table: OrmTable<*>): String {
+        val stream = StringBuilder()
+
+        stream.append(dumpTable(table))
+        stream.append("\n|  Dependency tables : " + table.dependencyTables.joinToString(prefix = "\n") { dumpTable(it).prependIndent("|\t\t") })
+
+        return stream.toString()
+    }
+
+    private fun dumpTable(table: Table): String {
+        val stream = StringBuilder()
+
+        fun dumpFk(forTable: Table, fk: ForeignKeyConstraint): String {
+            val fromTableName = fk.from.table.tableName
+
+            return "${if (fromTableName == forTable.tableName) "self" else fromTableName}(${fk.from.name}) -> ${fk.target.table.tableName}(${fk.target.name})"
+        }
+
+        stream.append("[Table \"${table.tableName}\"]")
+        stream.append("\n|  Columns : " + table.columns.map { "${it.columnType::class.simpleName} - ${it.name}"})
+        stream.append("\n|  FKs : " + table.columns.filter { it.foreignKey != null }
+            .map { dumpFk(table, it.foreignKey!!) })
+
+        return stream.toString()
     }
 
 }
