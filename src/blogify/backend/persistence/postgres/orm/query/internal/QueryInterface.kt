50865ee3ac699fef62088b896e20f2a9355468ab
benjozork <benjamin.dupont.984@gmail.com>
2020-03-30 16:39:14 -0400

    orm: more work on select fields

--- src/blogify/backend/persistence/postgres/orm/query/internal/QueryInterface.kt
+++ src/blogify/backend/persistence/postgres/orm/query/internal/QueryInterface.kt
@@ -5,6 +5,7 @@ import blogify.backend.persistence.postgres.orm.models.PropertyMapping
 import blogify.backend.resources.models.Resource
 
 import org.jetbrains.exposed.sql.Join
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Query
 import org.jetbrains.exposed.sql.FieldSet
 
@@ -26,13 +27,18 @@ class QueryInterface<TResource : Resource>(val table: OrmTable<TResource>) {
     /**
      * Finds the [FieldSet] needed to perform a `select` [Query] that covers the provided [mappings][PropertyMapping]
      *
-     * @param mappings the list of mappings that need to be selected by the query. If any value mappings from other classes
-     *                 are present, the associative mapping to that class from [klass] **must** be provided with it.
+     * @param mappings         the list of mappings that need to be selected by the query. If any value mappings from other classes
+     *                         are present, the associative mapping to that class from [klass] **must** be provided with it.
+     *
+     * @param pruneImplicit    whether or not fields implicitly added by any joins should be removed to only keep explicitly specified ones.
+     *                         Defaults to `true`. Can bee changed to `false` to get all mappings in a joined table.
+     *
+     * @param keepImplicitKeys if [pruneImplicit] is set to `true`, this parameter will keep FKs of [table] point to the PK of a joined table
      *
      * @return a [FieldSet] of all the [expressions][org.jetbrains.exposed.sql.Expression] involved, including necessary joins.
      *         the [FieldSet.source] property of the return value will be a [Join].
      */
-    fun findFieldsForMappings(mappings: List<PropertyMapping>): FieldSet {
+    fun findFieldsForMappings(mappings: List<PropertyMapping>, pruneImplicit: Boolean = true, keepImplicitKeys: Boolean = false): FieldSet {
         require (
             mappings.map { it.handle.klass }.let { klasses ->
                 klasses.all { it == table.klass || mappings.any { mapping -> mapping is PropertyMapping.AssociativeMapping<*> && mapping.dependency == it } }
@@ -46,7 +52,18 @@ class QueryInterface<TResource : Resource>(val table: OrmTable<TResource>) {
 
         // Of all the value mappings in the join, only keep the initially provided ones
 
-        return join.slice(mappings.filterIsInstance<PropertyMapping.ValueMapping>().map { it.column })
+        return if (pruneImplicit) {
+            if (keepImplicitKeys) {
+                join.slice(join.fields.filter {
+                    if (it !is Column<*>) return@filter false
+
+                    val isFkFromTableToPkOfJoinedTable = it.foreignKey != null && it.foreignKey!!.from.table == this.table && it.foreignKey!!.target.indexInPK != null
+                    val isInExplicitMappings           = it in mappings.filterIsInstance<PropertyMapping.ValueMapping>().map { m -> m.column }
+
+                    isFkFromTableToPkOfJoinedTable || isInExplicitMappings
+                })
+            } else join.slice(mappings.filterIsInstance<PropertyMapping.ValueMapping>().map { it.column })
+        } else join
     }
 
 }
