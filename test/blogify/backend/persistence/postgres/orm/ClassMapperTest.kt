00567c31fa374b1307788c558f916075886fe068
benjozork <benjamin.dupont.984@gmail.com>
2020-03-30 22:14:43 -0400

    orm: tests: move util functions

--- test/blogify/backend/persistence/postgres/orm/ClassMapperTest.kt
+++ test/blogify/backend/persistence/postgres/orm/ClassMapperTest.kt
@@ -4,10 +4,9 @@ import blogify.backend.annotations.Invisible
 import blogify.backend.persistence.postgres.orm.models.Cardinality
 import blogify.backend.persistence.postgres.orm.models.CollectionCardinality
 import blogify.backend.persistence.postgres.orm.annotations.Cardinality as CardinalityAnnotation
-import blogify.backend.persistence.postgres.orm.models.OrmTable
 import blogify.backend.persistence.postgres.orm.models.PropertyMapping
 import blogify.backend.resources.models.Resource
-import com.andreapivetta.kolor.*
+import blogify.backend.testutils.dumpOrmTable
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -270,60 +269,5 @@ class ClassMapperTest {
 //
 //        classes.forEach { println(dumpOrmTable(it) + "\n") }
 //    } */ */ */
-
-    companion object {
-
-        fun dumpExpression(expression: Expression<*>, verbose: Boolean = false): String {
-            return when (expression) {
-                is Column<*> -> dumpColumn(expression, verbose)
-                else -> "_$expression"
-            }
-        }
-
-        fun dumpColumn(column: Column<*>, verbose: Boolean = false): String =
-            (if (verbose) "${column.table.tableName.lightMagenta()}(" else "") +
-            column.let { when {
-                it.indexInPK != null   -> it.name.lightRed()
-                it.columnType.nullable -> "${it.name}?".lightBlue()
-                else -> it.name.lightGreen()
-            } } +
-            (if (verbose) "): " else ": ") +
-            column.columnType::class.simpleName?.cyan()
-
-        fun dumpFk(forTable: Table, fk: ForeignKeyConstraint): String {
-            val fromTableName = fk.from.table.tableName
-
-            return "${if (fromTableName == forTable.tableName) "self".lightMagenta() else fromTableName.lightMagenta()}(${fk.from.name.lightGreen()})" +
-                    " -> ${fk.target.table.tableName.lightMagenta()}(${fk.target.name.lightGreen()})"
-        }
-
-        fun dumpTable(table: Table): String {
-            val stream = StringBuilder()
-
-            stream.append("[Table ${table.tableName.lightMagenta()}]")
-            stream.append("\n|  Columns : " + table.columns.sortedBy { if (it.indexInPK != null) 0 else 1 }.map { dumpColumn(it) })
-            stream.append("\n|  FKs :     " +
-                    table.columns
-                        .mapNotNull { it.foreignKey }
-                        .map { dumpFk(table, it) }
-                        .ifEmpty { "[${"<none>".yellow()}]" }
-            )
-            return stream.toString()
-        }
-
-        fun dumpOrmTable(table: OrmTable<*>): String {
-            val stream = StringBuilder()
-
-            stream.append(dumpTable(table))
-            stream.append("\n|  Dep. tables : " +
-                    if (table.dependencyTables.isNotEmpty())
-                        table.dependencyTables.joinToString(prefix = "\n") { dumpTable(it).prependIndent("|\t\t") }
-                    else "<none>".yellow()
-            )
-
-            return stream.toString()
-        }
-
-    }
 
 }
