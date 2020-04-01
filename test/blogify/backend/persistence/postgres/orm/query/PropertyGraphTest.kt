adb61304c19010029e9ad9a8b17a0c802ea447b5
benjozork <benjamin.dupont.984@gmail.com>
2020-04-01 14:33:19 -0400

    orm: property-graph: fix resolve, add debug prints, improve pointers

--- test/blogify/backend/persistence/postgres/orm/query/PropertyGraphTest.kt
+++ test/blogify/backend/persistence/postgres/orm/query/PropertyGraphTest.kt
@@ -1,12 +1,25 @@
 package blogify.backend.persistence.postgres.orm.query
 
+import blogify.backend.persistence.postgres.orm.query.extensions.then
 import blogify.backend.persistence.postgres.orm.query.models.PropertyGraph
-import blogify.backend.persistence.postgres.orm.query.models.PropertyPointer
+import blogify.backend.persistence.postgres.orm.query.models.Pointer
+import blogify.backend.resources.Article
+import blogify.backend.resources.Comment
+import blogify.backend.resources.User
 import blogify.backend.resources.models.Resource
+import blogify.backend.resources.reflect.cachedPropMap
 import blogify.backend.resources.reflect.models.ext.okHandle
+import blogify.backend.testutils.PropertyGraphUtils
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+import kotlin.time.milliseconds
+
+import com.andreapivetta.kolor.lightBlue
+import com.andreapivetta.kolor.yellow
 
 class PropertyGraphTest {
 
@@ -25,26 +38,39 @@ class PropertyGraphTest {
         val breed: String
     ) : Resource()
 
+    @ExperimentalTime
     @Test fun `should make graph with simple pointers`() {
-        val houseOccupant = PropertyPointer<House, House, Owner>(null, House::occupant.okHandle())
-        val houseOccupantName = PropertyPointer<House, Owner, String>(houseOccupant, Owner::name.okHandle())
-        val houseOccupantCat = PropertyPointer<House, Owner, Cat>(houseOccupant, Owner::cat.okHandle())
-        val houseOccupantCatName = PropertyPointer<House, Cat, String>(houseOccupantCat, Cat::name.okHandle())
-        val houseOccupantCatBreed = PropertyPointer<House, Cat, String>(houseOccupantCat, Cat::breed.okHandle())
+        House::class.cachedPropMap()
+        Owner::class.cachedPropMap()
+        Cat::class.cachedPropMap()
 
-        val graph = PropertyGraph (
-            House::class,
-            houseOccupant,
-            houseOccupantName,
-            houseOccupantCat,
-            houseOccupantCatName,
-            houseOccupantCatBreed
-        )
+        val timeTaken = measureTime {
+            val houseOccupant = Pointer<House, House, Owner>(null, House::occupant)
+            val houseOccupantName = houseOccupant then Owner::name
+            val houseOccupantCat = houseOccupant then Owner::cat
+            val houseOccupantCatName = houseOccupantCat then Cat::name
+            val houseOccupantCatBreed = houseOccupantCat then Cat::breed
 
-        assertTrue(graph.rootChildren.map { it.pointer.handle.name }.containsAll(listOf("occupant")))
-        assertTrue(graph.rootChildren.first().children.map { it.pointer.handle.name }.containsAll(listOf("name", "cat")))
-        assertTrue(graph.rootChildren.first().children.first { it.pointer.handle.name == "cat" }.children.map { it.pointer.handle.name }
-            .containsAll(listOf("name", "breed")))
+            val graph = PropertyGraph (
+                House::class,
+                houseOccupant,
+                houseOccupantName,
+                houseOccupantCat,
+                houseOccupantCatName,
+                houseOccupantCatBreed
+            )
+
+            println(PropertyGraphUtils.dumpPropertyGraph(graph))
+
+            assertTrue(graph.rootChildren.map { it.pointer.handle.name }.containsAll(listOf("occupant")))
+            assertTrue(graph.rootChildren.first().children.map { it.pointer.handle.name }.containsAll(listOf("name", "cat")))
+            assertTrue(graph.rootChildren.first().children.first { it.pointer.handle.name == "cat" }.children.map { it.pointer.handle.name }
+                .containsAll(listOf("name", "breed")))
+        }
+
+        println("Time taken : ".yellow() + timeTaken.toString().lightBlue())
+
+        assertTrue(timeTaken < 10.milliseconds)
     }
 
     private data class PartnerContainer (
@@ -56,11 +82,11 @@ class PropertyGraphTest {
         val spouse: Partner?
     ) : Resource()
 
-    @Test fun `should make graph with complex pointers`() {
-        val partner = PropertyPointer<PartnerContainer, PartnerContainer, Partner>(null, PartnerContainer::contained.okHandle())
-        val partnerName = PropertyPointer<PartnerContainer, Partner, String>(partner, Partner::name.okHandle())
-        val partnerSpouse = PropertyPointer<PartnerContainer, Partner, Partner>(partner, Partner::spouse.okHandle())
-        val partnerSpouseName = PropertyPointer<PartnerContainer, Partner, String>(partnerSpouse, Partner::name.okHandle())
+    @Test fun `should make graph with circular reference pointers`() {
+        val partner = Pointer<PartnerContainer, PartnerContainer, Partner>(null, PartnerContainer::contained.okHandle())
+        val partnerName = Pointer<PartnerContainer, Partner, String>(partner, Partner::name.okHandle())
+        val partnerSpouse = Pointer<PartnerContainer, Partner, Partner>(partner, Partner::spouse.okHandle())
+        val partnerSpouseName = Pointer<PartnerContainer, Partner, String>(partnerSpouse, Partner::name.okHandle())
 
         val graph = PropertyGraph (
             PartnerContainer::class,
@@ -70,10 +96,32 @@ class PropertyGraphTest {
             partnerSpouseName
         )
 
+        println(PropertyGraphUtils.dumpPropertyGraph(graph))
+
         assertTrue(graph.rootChildren.map { it.pointer.handle.name }.containsAll(listOf("contained")))
         assertTrue(graph.rootChildren.first().children.map { it.pointer.handle.name }.containsAll(listOf("name", "spouse")))
         assertTrue(graph.rootChildren.first().children.first { it.pointer.handle.name == "spouse" }.children.map { it.pointer.handle.name }
             .contains("name"))
+    }
+
+    @Test fun a() {
+        class CommentContainer(val comment: Comment) : Resource()
+        val comment = Pointer<CommentContainer, CommentContainer, Comment>(null, CommentContainer::comment)
+        val commentAuthor = comment then Comment::commenter
+        val commentAuthorUsername = commentAuthor then User::username
+        val commentArticle = comment then Comment::article
+        val commentArticleTitle = commentArticle then Article::title
+
+        val graph = PropertyGraph (
+            CommentContainer::class,
+            comment,
+            commentAuthor,
+            commentAuthorUsername,
+            commentArticle,
+            commentArticleTitle
+        )
+
+        println(PropertyGraphUtils.dumpPropertyGraph(graph))
     }
 
 }
