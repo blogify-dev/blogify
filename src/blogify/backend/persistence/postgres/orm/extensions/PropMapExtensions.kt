c922a92e8fb965ac663cd474afb8556df0fef200
benjozork <benjamin.dupont.984@gmail.com>
2020-04-03 21:53:28 -0400

    orm: docs + unused imports

--- src/blogify/backend/persistence/postgres/orm/extensions/PropMapExtensions.kt
+++ src/blogify/backend/persistence/postgres/orm/extensions/PropMapExtensions.kt
@@ -4,22 +4,30 @@ import blogify.backend.persistence.postgres.orm.ClassMapper
 import blogify.backend.persistence.postgres.orm.models.OrmTable
 import blogify.backend.resources.models.Resource
 import blogify.backend.resources.reflect.models.PropMap
-import blogify.backend.resources.reflect.models.ext.handle
 import blogify.backend.util.letCatchingOrNull
 
 import java.util.concurrent.ConcurrentHashMap
 
 import kotlin.reflect.KClass
-import kotlin.reflect.KProperty1
 
 import com.andreapivetta.kolor.red
 
 private val mappedClasses = ConcurrentHashMap<KClass<*>, OrmTable<*>>()
 
+/**
+ * Retrieves an [OrmTable] for the receiver class. Intensive operation, cached after first access.
+ *
+ * @author Benjozork
+ */
 @Suppress("UNCHECKED_CAST")
 val <TResource : Resource> KClass<TResource>.mappedTable: OrmTable<TResource>
     get() = mappedClasses.getOrPut(this) { ClassMapper.mapClasses(this, mappedClasses = mappedClasses).first { it.klass == this } } as OrmTable<TResource>
 
+/**
+ * Retrieves an [OrmTable] for the receiver class. Not cached. Requires an [OrmTable] to have already been mapped by [mappedTable].
+ *
+ * @author Benjozork
+ */
 val <TResource : Resource> PropMap.PropertyHandle<TResource>.mapping
     get() = letCatchingOrNull { this.klass.mappedTable }?.let {
         it.mappings.firstOrNull { m -> m.handle == this }
--- src/blogify/backend/persistence/postgres/orm/query/models/PointerPredicate.kt
+++ src/blogify/backend/persistence/postgres/orm/query/models/PointerPredicate.kt
@@ -6,16 +6,37 @@ import blogify.backend.resources.models.Resource
 
 import org.jetbrains.exposed.sql.*
 
+/**
+ * Represents a comparison operation between a [Pointer] and a value `<: Pointer's TLeft`.
+ */
 enum class Op {
     Less, LessOrEquals, Equals, Greater, GreaterOrEquals
 }
 
+/**
+ * Container for a simple [operation][Op] predicate on a [Pointer].
+ *
+ * This should *not* be used directly by DSL implementations.
+ *
+ * @param TLeftContainer the `TContainer` type of the pointer on [lhs]
+ * @param TLeft          the `TValue` type of the pointer on [lhs], and the supertype of [TRight]
+ * @param TRight         the type of the value on [rhs], a subtype of [TLeft]
+ *
+ * @param lhs the left-hand pointer of the operation. Must **not** point to a [Resource] property.
+ * @param op  the [operation][Op] to be run
+ * @param rhs the right-hand value of the operation
+ *
+ * @author Benjozork
+  */
 class PointerPredicate<TLeftContainer : Resource, TLeft : Any, TRight : TLeft> (
     private val lhs: Pointer<*, TLeftContainer, TLeft>,
     private val op: Op,
     private val rhs: Expression<TRight>
 ) {
 
+    /**
+     * Converts a [PointerPredicate] to an [`Expresion<Boolean>`][Expression]
+     */
     fun toExpr(): Expression<Boolean> = when (op) {
         Op.Less            -> LessOp((lhs.handle.mapping as PropertyMapping.ValueMapping).column, rhs)
         Op.LessOrEquals    -> LessEqOp((lhs.handle.mapping as PropertyMapping.ValueMapping).column, rhs)
