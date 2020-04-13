cd09973582b1ed0b28cf95bd3cb2c14382de42c6
benjozork <benjamin.dupont.984@gmail.com>
2020-04-12 21:58:10 -0400

    db: jsonb: add resource serializer (temp)

--- src/blogify/backend/util/Jsonb.kt
+++ src/blogify/backend/util/Jsonb.kt
@@ -1,14 +1,24 @@
 package blogify.backend.util
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import blogify.backend.resources.models.Resource
+
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ColumnType
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.statements.api.PreparedStatementApi
+
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
 import org.postgresql.util.PGobject
 
 
-private val objectMapper = jacksonObjectMapper()
+private val objectMapper = jacksonObjectMapper().apply {
+    val resourceModule = SimpleModule()
+    resourceModule.addSerializer(Resource.ResourceIdSerializer)
+
+    registerModule(resourceModule)
+}
 
 inline fun <reified T : Any> Table.jsonb(name: String): Column<T> =
     registerColumn(name, Json(T::class.java))
