e2062d649139ea56794c3c40fc9ae081ee2ca968
benjozork <benjamin.dupont.984@gmail.com>
2020-04-14 00:03:35 -0400

    misc: formatting

--- src/blogify/backend/util/Jsonb.kt
+++ src/blogify/backend/util/Jsonb.kt
@@ -7,11 +7,11 @@ import org.jetbrains.exposed.sql.ColumnType
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.statements.api.PreparedStatementApi
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 import org.postgresql.util.PGobject
-
 
 private val objectMapper = jacksonObjectMapper().apply {
     val resourceModule = SimpleModule()
