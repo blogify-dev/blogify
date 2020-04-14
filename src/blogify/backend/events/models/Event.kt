440e280f6820e531cd0ef57e4d4a3a68061212e0
benjozork <benjamin.dupont.984@gmail.com>
2020-04-14 00:02:13 -0400

    notifications: push-server: send all events, not just notifications

--- src/blogify/backend/events/models/Event.kt
+++ src/blogify/backend/events/models/Event.kt
@@ -3,6 +3,8 @@ package blogify.backend.events.models
 import blogify.backend.database.Notifications
 import blogify.backend.pipelines.wrapping.RequestContext
 import blogify.backend.resources.reflect.models.Mapped
+import blogify.backend.util.getOrNull
+import blogify.backend.util.never
 
 import java.time.Instant
 
@@ -16,9 +18,8 @@ open class Event (
 
     suspend fun send(request: RequestContext) {
         source.targets.forEach { it.sendEvent(request.appContext, this) }
-        if (this.type == EventType.Notification) {
-            Notifications.insert(this).get()
-        }
+
+        Notifications.insert(this).getOrNull() ?: never
     }
 
 }
