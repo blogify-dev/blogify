24f9f3736837fdfffd76b89a79d3c21ce48a8fc2
benjozork <benjamin.dupont.984@gmail.com>
2020-04-20 13:14:43 -0400

    utils: extensions: Introduce toUUIDOrNull()

--- src/blogify/backend/util/Conversions.kt
+++ src/blogify/backend/util/Conversions.kt
@@ -6,6 +6,8 @@ import java.util.*
 
 fun String.toUUID(): UUID = UUID.fromString(this)
 
+fun String.toUUIDOrNull(): UUID? = letCatchingOrNull { UUID.fromString(this) }
+
 fun UUID.short(): String = this.toString().takeLast(8)
 
 fun String.hash(): String = encoder.encode(this)
