4b0442b8b7809a38952eb37247f9693975628a3b
benjozork <benjamin.dupont.984@gmail.com>
2020-04-20 19:45:08 -0400

    frontend: caching: improve code

--- src/blogify/frontend/src/app/shared/services/state/state.service.ts
+++ src/blogify/frontend/src/app/shared/services/state/state.service.ts
@@ -1,34 +1,42 @@
 import { Injectable } from '@angular/core';
-import {Article} from "../../../models/Article";
-import {User} from "../../../models/User";
+import { Article } from '../../../models/Article';
+import { User } from '../../../models/User';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StateService {
+
     // uuid: Article
-    private articles: { [key: string]: Article } = {  }
+    private articles: { [key: string]: Article } = {};
 
     // uuid: User
-    private users: { [key: string]: User } = {}
+    private users: { [key: string]: User } = {};
 
     constructor() {}
 
     getArticle(uuid: string): Article | null {
-        const article = this.articles[uuid]
-        return article !== undefined ? article : null
+        return this.articles[uuid] ?? null;
     }
 
     cacheArticle(article: Article) {
-        this.articles[article.uuid] = article
+        this.articles[article.uuid] = article;
+    }
+
+    clearArticles() {
+        this.articles = {};
     }
 
     getUser(uuid: string): User | null {
-        const user = this.users[uuid]
-        return user !== undefined ? user : null
+        return this.users[uuid] ?? null;
     }
 
     cacheUser(user: User) {
-        this.users[user.uuid] = user
+        this.users[user.uuid] = user;
     }
+
+    clearUsers() {
+        this.users = {};
+    }
+
 }
