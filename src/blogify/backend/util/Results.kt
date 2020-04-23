ac9d6b7114388b38f6eb578f584aafcb826d960e
Benjamin <benjamin.dupont.984@gmail.com>
2020-04-23 17:33:02 -0400

    utils: results: introduce assertGet() and add docs

--- src/blogify/backend/util/Results.kt
+++ src/blogify/backend/util/Results.kt
@@ -1,5 +1,6 @@
 package blogify.backend.util
 
+import blogify.backend.pipelines.PipelineException
 import blogify.backend.pipelines.pipelineError
 
 import io.ktor.http.HttpStatusCode
@@ -13,12 +14,27 @@ open class BException(causedBy: Exception) : Exception(causedBy)
 typealias Sr<V> = SuspendableResult<V, Exception>
 typealias SrList<V> = SuspendableResult<List<V>, Exception>
 
+/**
+ * Wraps a generic producer function in a [Sr]
+ *
+ * @author Benjozork
+ */
 @Suppress("FunctionName")
 suspend fun <T : Any> Wrap(producer: suspend () -> T): Sr<T> = Sr.of(producer)
 
+/**
+ * Wraps a generic producer function in a [Sr] without suspending
+ *
+ * @author Benjozork
+ */
 @Suppress("FunctionName")
 fun <T : Any> WrapBlocking(producer: suspend () -> T): Sr<T> = runBlocking { Sr.of<T, Exception>(producer) }
 
+/**
+ * Fetches the value of a [Result] or throws a [PipelineException] if the result is a [Result.Failure]
+ *
+ * @author Benjozork
+ */
 fun <V : Any, E : Exception> SuspendableResult<V, E>.getOrPipelineError (
     code:    HttpStatusCode = HttpStatusCode.InternalServerError,
     message: String = "error while fetching generic result"
@@ -29,11 +45,29 @@ fun <V : Any, E : Exception> SuspendableResult<V, E>.getOrPipelineError (
     }
 }
 
-suspend fun <V : Any, E : Exception> SuspendableResult<V, E>.getOr(block: suspend (E) -> Nothing): V =
+/**
+ * Fetches the value of a [Result] or returns the result of [block] with the error if the result is a [Result.Failure]
+ *
+ * @author Benjozork
+ */
+suspend fun <V : Any, E : Exception> SuspendableResult<V, E>.getOr(block: suspend (E) -> V): V =
     when (this) {
         is SuspendableResult.Success -> get()
         is SuspendableResult.Failure -> block(this.error)
     }
 
+/**
+ * Fetches the value of a [Result] or returns `null` if the result is a [Result.Failure]
+ *
+ * @author Benjozork
+ */
 fun <V : Any, E : Exception> SuspendableResult<V, E>.getOrNull(): V? =
     if (this is SuspendableResult.Success) get() else null
+
+/**
+ * Fetches the value of a [Result] or evaluates [never] if the result is a [Result.Failure]
+ *
+ * @author Benjozork
+ */
+fun <V : Any, E : Exception> SuspendableResult<V, E>.assertGet(): V =
+    if (this is SuspendableResult.Success) get() else never
