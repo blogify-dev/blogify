8373c460895ed70593d6225d995e72b87cf24402
Hamza <muhammadhamza1311@gmail.com>
2020-04-24 14:18:26 +0500

    misc: remove unused service import

--- src/blogify/frontend/src/app/app.component.ts
+++ src/blogify/frontend/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { AuthService } from './shared/services/auth/auth.service'
 import { CommentsService } from './services/comments/comments.service'
 import { ArticleService } from './services/article/article.service'
 import { NotificationsService } from './shared/services/notifications/notifications.service'
-import { ToastrService } from 'ngx-toastr'
 import { StaticContentService } from './services/static/static-content.service'
 import { Router } from '@angular/router'
 
@@ -19,7 +18,6 @@ export class AppComponent implements OnInit {
         public authService: AuthService,
         private commentsService: CommentsService,
         private articleService: ArticleService,
-        private toastrService: ToastrService,
         private notificationsService: NotificationsService,
         private staticContentService: StaticContentService,
         private router: Router
