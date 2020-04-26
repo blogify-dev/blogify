ea7f5b8e090984db75d5095c043ed21572597d0f
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-04-25 23:04:49 -0400

    tests: integration: run register test before login, use same username / password

--- src/blogify/frontend/cypress/integration/auth_spec.ts
+++ src/blogify/frontend/cypress/integration/auth_spec.ts
@@ -1,47 +1,50 @@
 describe('Authentication Test', () => {
     beforeEach(() => {
-        cy.viewport(1920, 1080)
-    })
+        cy.viewport(1920, 1080);
+    });
 
-    it('should log the user in', () => {
-        const { username, password } = { username: 'user', password: 'pass' }
-
-        cy.visit('login?redirect=%2Fhome')
-
-        cy.get('#login-input-username').type(username)
-
-        cy.get('#login-input-password').type(password)
-
-        cy.get('button').contains('Login').click()
-
-        cy.get('#profile')
-            .get('.user-display')
-            .get('.display-info')
-            .get('.primary-info')
-            .should('contain.text', username)
-    })
+    let registeredUser: string;
+    let registeredPass: string;
 
     it('should register a new user', () => {
-        cy.visit('login?redirect=%2Fhome')
+        cy.visit('login?redirect=%2Fhome');
 
-        cy.get('h2').contains('Register').click()
+        cy.get('h2').contains('Register').click();
 
-        const username = Math.random().toString(36).substring(7)
+        registeredUser = Math.random().toString(36).substring(7);
+        registeredPass = Math.random().toString(36).substring(7);
 
-        cy.get('#register-input-username').type(username)
+        cy.get('#register-input-username').type(registeredUser);
 
-        cy.get('#register-input-password').type('password')
+        cy.get('#register-input-password').type(registeredPass);
 
-        cy.get('#register-input-name').type('the name')
+        cy.get('#register-input-name').type('the name');
 
-        cy.get('#register-input-email').type('email@bruh.com')
+        cy.get('#register-input-email').type('email@bruh.com');
 
-        cy.get('button').contains('Register').click()
+        cy.get('button').contains('Register').click();
 
         cy.get('#profile')
             .get('.user-display')
             .get('.display-info')
             .get('.primary-info')
-            .should('contain.text', username)
-    })
-})
+            .should('contain.text', registeredUser);
+    });
+
+    it('should log the user in', () => {
+        cy.visit('login?redirect=%2Fhome');
+
+        cy.get('#login-input-username').type(registeredUser);
+
+        cy.get('#login-input-password').type(registeredPass);
+
+        cy.get('button').contains('Login').click();
+
+        cy.get('#profile')
+            .get('.user-display')
+            .get('.display-info')
+            .get('.primary-info')
+            .should('contain.text', registeredUser);
+    });
+
+});
