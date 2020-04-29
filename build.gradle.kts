0f28b448f74a7b4813c20e7baffbaabbf135b0f3
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-04-29 01:51:46 -0400

    build: use epgx version alpha-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.zaxxer:HikariCP:$hikari_version")
-    implementation("com.github.Benjozork:exposed-postgres-extensions:4326e437")
+    implementation("com.github.Benjozork:exposed-postgres-extensions:alpha-1")
 
     // Spring security for hashing
 
