fc4107e167565383ffc85efd8a68d3f325b0cca0
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-05-01 00:58:49 -0400

    users: settings: use companion object for jsonb converter

--- src/blogify/backend/resources/user/UserSettings.kt
+++ src/blogify/backend/resources/user/UserSettings.kt
@@ -1,7 +1,10 @@
 package blogify.backend.resources.user
 
+import blogify.backend.appContext
 import blogify.backend.resources.static.models.StaticFile
 import blogify.reflect.models.Mapped
+
+import epgx.types.Jsonb
 
 import io.ktor.http.ContentType
 
@@ -11,4 +14,6 @@ data class UserSettings (
 
     val coverPictureFile: StaticFile = StaticFile.None(ContentType.Image.Any)
 
-) : Mapped()
+) : Mapped() {
+    companion object : Jsonb.Converter<UserSettings> by appContext.createJsonbConverter()
+}
