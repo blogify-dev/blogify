e38709ca2076397cfbb162a5d7cfecf0ca8270dd
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-05-16 00:00:08 -0400

    reflect: computedProps2: docs, remove wasResolveAttempted

--- reflect/src/blogify/reflect/computed/models/ComputedPropContainer.kt
+++ reflect/src/blogify/reflect/computed/models/ComputedPropContainer.kt
@@ -2,19 +2,37 @@ package blogify.reflect.computed.models
 
 import blogify.reflect.models.Mapped
 
+/**
+ * Represents a container holding a computed property, which may or may not be resolved.
+ *
+ * @property obj        the instance of [TMapped] this computed property is attached to
+ * @property resolution the resolution state of the computed property
+ *
+ * @author Benjozork
+ */
 abstract class ComputedPropContainer <TMapped : Mapped, TProperty : Any?> {
 
     abstract val obj: TMapped
 
     var resolution: Resolution = Resolution.Unresolved
 
-    var wasResolveAttempted = false
-
+    /**
+     * Represents the resolution state of the computed property
+     */
     sealed class Resolution {
+        /**
+         * Used when a computed property's value is known and resolved
+         */
         class Value<TProperty : Any?>(val value: TProperty) : Resolution()
 
+        /**
+         * Used when a computed property's value is unknown, but there was a resolution attempt
+         */
         object Undefined : Resolution()
 
+        /**
+         * Used when a computed property's value is unknown, and there was no resolution attempt
+         */
         object Unresolved : Resolution()
 
         override fun equals(other: Any?): Boolean {
