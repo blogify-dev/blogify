8fafe95c665ac7b9d210d1bd7eb2f91265ef69d5
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-05-18 16:13:39 -0400

    filtering: allow matching a filter against an instance of its TMapped

--- filtering/src/blogify/filtering/Filter.kt
+++ filtering/src/blogify/filtering/Filter.kt
@@ -1,12 +1,29 @@
 package blogify.filtering
 
+import blogify.reflect.SlicedProperty
+import blogify.reflect.extensions.okHandle
+import blogify.reflect.getPropValueOnInstance
 import blogify.reflect.models.Mapped
+import blogify.reflect.models.PropMap
 
 import kotlin.reflect.KProperty1
 
-abstract class Filter<TMapped : Mapped, TProperty : Any?>(property: KProperty1<TMapped, TProperty>) {
+abstract class Filter<TMapped : Mapped, TProperty : Any?>(val property: KProperty1<TMapped, TProperty>) {
+
+    init {
+        assert(property.okHandle is PropMap.PropertyHandle.Valid) { "cannot create a filter for a property that doesn't have a Valid handle in a safe propMap" }
+    }
 
     abstract infix fun matchesFor(value: TProperty): Boolean
+
+    @Suppress("UNCHECKED_CAST")
+    infix fun matchesFor(instance: TMapped): Boolean {
+        return when (val valueOnInstance = getPropValueOnInstance(instance, property.name)) {
+            is SlicedProperty.Value -> matchesFor(valueOnInstance.value as TProperty)
+            is SlicedProperty.NullableValue -> matchesFor(valueOnInstance.value as TProperty)
+            else -> false
+        }
+    }
 
     class IsValue<TMapped : Mapped, TProperty : Any?> (
         property: KProperty1<TMapped, TProperty>,
