8816e3fb22bb999bd4f08368232686d6f10c8435
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-05-18 16:13:39 -0400

    filtering: allow matching a filter against an instance of its TMapped

--- filtering/test/blogify/filtering/FilterTest.kt
+++ filtering/test/blogify/filtering/FilterTest.kt
@@ -1,20 +1,36 @@
 package blogify.filtering
 
+import blogify.reflect.annotations.Hidden
 import blogify.reflect.models.Mapped
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
+import java.lang.IllegalStateException
+
 class FilterTest {
 
-    @Test fun `should work with simple filter`() {
+    @Test fun `should work with simple filters`() {
         class Test(val x: String) : Mapped()
 
         val filter = Filter.IsValue(Test::x, "hi !")
-        val notFilter = Filter.IsNotValue(Test::x, "hello !")
+        val notFilter = Filter.IsNotValue(Test::x, "hi !")
 
         assertTrue(filter matchesFor "hi !")
-        assertTrue(notFilter matchesFor "hi !")
+        assertFalse(notFilter matchesFor "hi !")
+
+        val test = Test("hi !")
+
+        assertTrue(filter matchesFor test)
+        assertFalse(notFilter matchesFor test)
+    }
+
+    @Test fun `should not be able to create filter for invalid handle`() {
+        class Test(@Hidden val x: String) : Mapped()
+
+        assertThrows(IllegalStateException::class.java) {
+            Filter.IsValue(Test::x, "hi !")
+        }
     }
 
 }
