a51420f426e356464144a47f7d84812ddb23738c
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-05-21 01:30:06 -0400

    db: sql binding: fix typo + change name

--- src/blogify/backend/database/binding/SqlBinding.kt
+++ src/blogify/backend/database/binding/SqlBinding.kt
@@ -52,11 +52,11 @@ sealed class SqlBinding<TResource : Resource, TProperty : Any?, TColumn : Any?> 
     ) : SqlBinding<TResource, Collection<TProperty>, UUID>(table, null, property) {
 
         @Suppress("UNCHECKED_CAST")
-        private val redTableIdentityCol = otherTable.columns.firstOrNull { it.referee == table.uuid } as? Column<UUID>
+        private val otherTableFktoPkCol = otherTable.columns.firstOrNull { it.referee == table.uuid } as? Column<UUID>
             ?: error("cannot make a ReferenceToMany SqlBinding: no foreign key from target table to PK of origin table")
 
         override val selectSubQuery: (UUID) -> Query =
-            { uuid -> otherTable.select { redTableIdentityCol eq uuid } }
+            { uuid -> otherTable.select { otherTableFktoPkCol eq uuid } }
 
     }
 
