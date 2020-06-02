3eecc56d888aeb76f95dd7e966ede1b6a32b1978
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-06-02 14:16:58 -0400

    core-reflect: new slicer

---
+++ src/blogify/backend/resources/reflect/Slicer.kt
@@ -0,0 +1,29 @@
+package blogify.backend.resources.reflect
+
+import blogify.backend.resources.reflect.metadata.entity
+import blogify.reflect.Dto
+import blogify.reflect.analysis.extensions.descriptor
+import blogify.reflect.models.Mapped
+
+import kotlin.reflect.KProperty1
+
+inline fun <reified TMapped : Mapped> TMapped.slice (
+    selectedProperties: Array<out KProperty1<TMapped, Any?>> = emptyArray(),
+    unsafe: Boolean = false
+): Dto {
+    val props = TMapped::class.descriptor.propertyDescriptors
+    val dto = mutableMapOf<String, Any?>()
+
+    val propIterator =
+        if (selectedProperties.isEmpty() || unsafe)
+            props
+        else
+            props.filter { it.key.name in selectedProperties.map { p -> p.name } }
+
+    for (prop in propIterator) {
+        if (prop.value.entity.isVisible)
+            dto[prop.key.name] = prop.key.get(this)
+    }
+
+    return dto
+}
