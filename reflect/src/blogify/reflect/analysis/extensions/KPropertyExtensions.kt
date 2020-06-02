b53f740c1efa11d3206522b87585fe5247e731e2
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-06-02 14:06:58 -0400

    reflect: metadata: better types

--- reflect/src/blogify/reflect/analysis/extensions/KPropertyExtensions.kt
+++ reflect/src/blogify/reflect/analysis/extensions/KPropertyExtensions.kt
@@ -1,9 +1,10 @@
 package blogify.reflect.analysis.extensions
 
+import blogify.reflect.analysis.models.PropertyDescriptor
 import blogify.reflect.extensions.klass
 import blogify.reflect.models.Mapped
 
 import kotlin.reflect.KProperty1
 
-val KProperty1<out Mapped, *>.descriptor get() =
+val KProperty1<out Mapped, Any?>.descriptor: PropertyDescriptor get() =
     this.klass.descriptor.propertyDescriptors[this]!!
--- reflect/src/blogify/reflect/analysis/models/ClassDescriptor.kt
+++ reflect/src/blogify/reflect/analysis/models/ClassDescriptor.kt
@@ -1,15 +1,18 @@
 package blogify.reflect.analysis.models
 
 import blogify.reflect.analysis.models.metadata.Metadata
+import blogify.reflect.models.Mapped
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 
 class ClassDescriptor(val klass: KClass<*>) : ElementDescriptor<KClass<*>>() {
 
     override val storedMetadata = mutableMapOf<KClass<out Metadata<KClass<*>>>, Metadata<KClass<*>>>()
 
+    @Suppress("UNCHECKED_CAST")
     val propertyDescriptors = klass.declaredMemberProperties.associateWith { PropertyDescriptor(it) }
-        .toMutableMap()
+        .toMutableMap() as MutableMap<KProperty1<Mapped, Any?>, PropertyDescriptor>
 
 }
