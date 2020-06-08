f966ea408dde1528b9671606d9a0c7f09031713b
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-06-07 22:19:37 -0400

    reflect: metadata: make ClassMetadata accept KClass<out Mapped>

--- reflect/src/blogify/reflect/analysis/models/metadata/ClassMetadata.kt
+++ reflect/src/blogify/reflect/analysis/models/metadata/ClassMetadata.kt
@@ -1,5 +1,7 @@
 package blogify.reflect.analysis.models.metadata
 
+import blogify.reflect.models.Mapped
+
 import kotlin.reflect.KClass
 
-interface ClassMetadata : Metadata<KClass<*>>
+interface ClassMetadata : Metadata<KClass<out Mapped>>
