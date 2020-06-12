1dec8e1b8a83017caba47236db847778048e1fe5
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-06-11 22:58:25 -0400

    reflect-entity: add entity class metadata

--- reflect-entity/src/blogify/reflect/entity/metadata/PropertyDescriptorExtensions.kt
+++ reflect-entity/src/blogify/reflect/entity/metadata/PropertyDescriptorExtensions.kt
@@ -1,6 +1,10 @@
 package blogify.reflect.entity.metadata
 
+import blogify.reflect.analysis.models.ClassDescriptor
 import blogify.reflect.analysis.models.PropertyDescriptor
 
 val PropertyDescriptor.entity get() =
     this.getOrMake(EntityMetadata.Provider)
+
+val ClassDescriptor.entity get() =
+    this.getOrMake(EntityClassMetadata.Provider)
--- reflect/src/blogify/reflect/annotations/search/NoSearch.kt
+++ reflect/src/blogify/reflect/annotations/search/NoSearch.kt
@@ -1,5 +1,5 @@
 package blogify.reflect.annotations.search
 
-@Target(AnnotationTarget.PROPERTY)
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class NoSearch
