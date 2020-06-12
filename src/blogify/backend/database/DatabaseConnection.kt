c44be405bf2488876561a90e915c9f1867e57ef7
Hamza <muhammadhamza1311@gmail.com>
2020-06-12 19:16:11 +0500

    cleanup: apply ftw

--- src/blogify/backend/database/DatabaseConnection.kt
+++ src/blogify/backend/database/DatabaseConnection.kt
@@ -13,23 +13,23 @@ import org.jetbrains.exposed.sql.Database
  */
 object DatabaseConnection {
 
-    private fun configureHikariCP(dbConfig: DatabaseConfig): HikariDataSource {
-        val config = HikariConfig()
-        config.driverClassName        = "org.postgresql.Driver"
-        config.jdbcUrl                = "jdbc:postgresql://${dbConfig.host}:${dbConfig.port}/${dbConfig.databaseName}"
-        config.maximumPoolSize        = 64
-        config.minimumIdle            = 6
-        config.validationTimeout      = 10 * 1000
-        config.connectionTimeout      = 10 * 1000
-        config.maxLifetime            = 30 * 60 * 1000
-        config.leakDetectionThreshold = 60 * 1000
-        config.isAutoCommit           = false
-        config.transactionIsolation   = "TRANSACTION_REPEATABLE_READ"
-        config.username               = dbConfig.username
-        config.password               = dbConfig.password
-        config.validate()
-        return HikariDataSource(config)
-    }
+    private fun configureHikariCP(dbConfig: DatabaseConfig): HikariDataSource =
+        HikariDataSource(HikariConfig().apply {
+            driverClassName        = "org.postgresql.Driver"
+            jdbcUrl                = "jdbc:postgresql://${dbConfig.host}:${dbConfig.port}/${dbConfig.databaseName}"
+            maximumPoolSize        = 64
+            minimumIdle            = 6
+            validationTimeout      = 10 * 1000
+            connectionTimeout      = 10 * 1000
+            maxLifetime            = 30 * 60 * 1000
+            leakDetectionThreshold = 60 * 1000
+            isAutoCommit           = false
+            transactionIsolation   = "TRANSACTION_REPEATABLE_READ"
+            username               = dbConfig.username
+            password               = dbConfig.password
+            validate()
+        })
+
 
     fun connect(config: DatabaseConfig = appContext.databaseConfig) = Database.connect(configureHikariCP(config))
 
